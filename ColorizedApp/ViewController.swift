97750b944da2eb17d22937c8b0ada0a57cf52cec
Birewkoss <birewkoff@gmail.com>
2024-05-16 15:43:34 +0300

    Сustomization of appearance

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -7,13 +7,69 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    // MARK: - IB Outlet
+    @IBOutlet var colorBord: UIView!
+    
+    @IBOutlet var redLabelForNumbers: UILabel!
+    @IBOutlet var greenLabelForNumbers: UILabel!
+    @IBOutlet var blueLabelForNumbers: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorBord.layer.cornerRadius = 20.0
+        redSetupSlider()
+        greenSetupSlider()
+        blueSetupSlider()
+        setupNumberForLabel()
+    }
+    
+    // MARK: - IB Action
+    @IBAction func redSliderAction() {
+        redLabelForNumbers.text = String(format: "%.2f", redSlider.value)
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenLabelForNumbers.text = String(format: "%.2f", greenSlider.value)
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueLabelForNumbers.text = String(format: "%.2f", blueSlider.value)
+    }
+    
+    // MARK: - Privat Func
+    private func setupNumberForLabel() {
+        redLabelForNumbers.text = String(format: "%.2f", redSlider.value)
+        greenLabelForNumbers.text = String(format: "%.2f", greenSlider.value)
+        blueLabelForNumbers.text = String(format: "%.2f", blueSlider.value)
     }
 
-
+    private func redSetupSlider() {
+        redSlider.value = 0
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
+        redSlider.minimumTrackTintColor = .red
+    }
+    
+    private func greenSetupSlider() {
+        greenSlider.value = 0
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 1
+        greenSlider.minimumTrackTintColor = .green
+    }
+    
+    private func blueSetupSlider() {
+        blueSlider.value = 0
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 1
+        blueSlider.minimumTrackTintColor = .blue
+    }
 }
 
